7bf1b100914475557b61cb707cccd62a69c7900f
VyanPersad <vyanpersad@gmail.com>
2022-03-27 00:38:25 -0400

    Added more files.
    Added social media functions (not facebook), insta and gmail.
    The site has a google account and email, with location and contact facilities, email and whatsapp.

---
+++ list.js
@@ -0,0 +1,75 @@
+//Appetizer Menu Display
+$(document).ready(function() {
+    $.getJSON('Menus/Appetizer.json', function(data) {
+        var output = ' ';
+        $.each(data.appetizer, function(key, value) {
+            output = output+ 
+            `<li class="card m-1 p-1"> 
+                <div class="row p-3">
+                    <div class="col-3">${value.item}</div>
+                    <div class="col-3">${value.name} </div>
+                    <div class="col-3">${value.serves}</div>
+                    <div class="col-3">${value.price}</div>
+                </div>
+            </li>`;
+        });$('#main').html(output);
+    });
+});
+
+//Main Menu Display
+$(document).ready(function() {
+    $.getJSON('Menus/Main.json', function(data) {
+        var output = ' ';
+        $.each(data.main, function(key, value) {
+            output = output+ 
+            `<li class="card m-1 p-1"> 
+                <div class="row p-3">
+                    <div class="col-1">${value.item}</div>
+                    <div class="col-2">${value.name}</div>
+                    <div class="col-4">${value.desc}</div>
+                    <div class="col-2">${value.serves}</div>
+                    <div class="col-3">${value.price}</div>
+                </div>
+            </li>`;
+        });
+        $('#main').html(output);
+    });
+});
+
+//Wine Display
+$(document).ready(function() {
+    $.getJSON('Menus/Wines.json', function(data) {
+        var output = ' ';
+        $.each(data.wine, function(key, value) {
+            output = output+ 
+            `<li class="card m-1 p-1"> 
+                <div class="row p-3">
+                    <div class="col-3">${value.item}</div>
+                    <div class="col-3">${value.name}</div>
+                    <div class="col-3">${value.serves}</div>
+                    <div class="col-3">${value.price}</div>
+                </div>
+            </li>`;
+        });
+        $('#wine').html(output);
+    });
+});
+
+//Desert Display
+$(document).ready(function() {
+    $.getJSON('Menus/Desert.json', function(data) {
+        var output = ' ';
+        $.each(data.desert, function(key, value) {
+            output = output+ 
+            `<li class="card m-1 p-1"> 
+                <div class="row p-3">
+                    <div class="col-3">${value.item}</div>
+                    <div class="col-3">${value.name}</div>
+                    <div class="col-3">${value.serves}</div>
+                    <div class="col-3">${value.price}</div>
+                </div>
+            </li>`;
+        });
+        $('#dese').html(output);
+    });
+});
